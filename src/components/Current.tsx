3acc0b4192e8eef6e1b115c580c2b71eb2f190fb
minmeeen <sirapat.mp@gmail.com>
2021-08-26 23:14:54 +0700

    done

--- src/components/Current.tsx
+++ src/components/Current.tsx
@@ -1,47 +1,69 @@
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react-router/node_modules/@types/react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { createTypePredicateNodeWithModifier } from 'typescript';
 
 
-// type Currprice = {
-//     // time.updated : string;
-//     // bpi.THB.rate_float : number;
-// }
+type CurrType = {
+    time:{
+        updated: string,
+        updatedISO: string,
+        updateduk: string,
+    },
+    disclaimer: string,
+    bpi:{
+        USD: {
+            code: string,
+            rate: string,
+            description: string,
+            rate_float: number
+
+        },
+        THB:{
+            code: string,
+            rate: string,
+            description: string,
+            rate_float: number
+        }
+
+    }
+}
 
 const Current = () => {
-    // const [price, setPrice] = useState<string>();
-    // const [loading, setLoading] = useState(true)
-    // const [error, setError] = useState(false)
+    const [price, setPrice] = useState<CurrType | null>(null);
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<boolean>(false)
 
-    // //fetch with async await
-    // const fetchApi = async () => {
-    //     try {
-    //         const resp =
-    //             await axios.get("https://api.coindesk.com/v1/bpi/currentprice/thb.json")
-    //         setPrice(resp.data)
-    //         setLoading(false)
-    //     }
-    //     catch (err) {
-    //         setLoading(false)
-    //         setError(true)
-    //     }
-    // }
+    //fetch with async await
+    const fetchApi = async () => {
+        try {
+            const resp =
+                await axios.get<CurrType>('https://api.coindesk.com/v1/bpi/currentprice/thb.json')
+            setPrice(resp.data)
+            setLoading(false)
+        }
+        catch (err) {
+            setLoading(false)
+            setError(true)
+        }
+    }
 
-    // useEffect(() => {
-    //     fetchApi()
-    // }, [])
+
+    useEffect(() => {
+        fetchApi()
+    }, [])
 
     const render = () => {
-        // if (loading)
-        //     return <p className='text-2xl'>Loading ... </p>
-        // else if (error)
-        //     return <p>There was some error !</p>
-        // else
+        if (loading)
+            return <p className='text-2xl'>Loading ... </p>
+        else if (error)
+            return <p>There was some error !</p>
+        else
             return (
                 <div>
-                    {/* <p>{price}</p> */}
-                    <p className='text-2xl'>{(90000000).toLocaleString()} THB</p>
-                    <p> (Last updated) </p>
+                    <p className='text-2xl'> {price?.bpi.THB.rate.toLocaleString()} THB</p>
+                    <br></br>
+                    <p> (Last updated {price?.time.updated}) </p>
                 </div>
 
             )
