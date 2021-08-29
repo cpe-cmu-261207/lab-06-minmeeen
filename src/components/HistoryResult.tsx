9a1c290e3faf8ff1dbbe365bcb2730afd2d48a5c
minmeeen <sirapat.mp@gmail.com>
2021-08-29 15:00:00 +0700

    done

--- src/components/HistoryResult.tsx
+++ src/components/HistoryResult.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import axios from 'axios';
 import { useEffect, useState } from 'react'
 import { useLocation } from "react-router-dom";
@@ -17,9 +16,10 @@ type PType = {
 
 
 const HistoryResult = () => {
-    const [price, setPrice] = useState<PType | null>(null);
+    const [price, setPrice] = useState<PType | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
-    const [error, setError] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false)
+    const [have, setHave] = useState<boolean>(false)
 
     function useQuery() {
         return new URLSearchParams(useLocation().search);
@@ -34,9 +34,10 @@ const HistoryResult = () => {
     const fetchApi = async () => {
         try {
             const resp =
-                await axios.get<PType>('https://api.coindesk.com/v1/bpi/historical/close.json?currency=THB&start=' + s + '&end=' + e)
-            setPrice(resp.data)
-            setLoading(false)
+                await axios.get<PType>(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=THB&start=${s}&end=${e}`)
+            setPrice(resp.data);
+            setLoading(false);
+            setHave(true)
         }
         catch (err) {
             setLoading(false)
@@ -44,27 +45,29 @@ const HistoryResult = () => {
         }
     }
 
-
     useEffect(() => {
         fetchApi()
     }, [])
 
-    const pr = () => {
 
-        let a = [];
-        for (const[key, value] of Object.entries(price?.bpi)) {
-            a.push(`${key} - ${Number(value).toLocaleString()} THB`);
-        }
-        const b = a.map(x => <ul className="nobull"><li> {x} </li></ul>)
-        return b;
+    const pr = () => {
+        
+            let a = [];
+            for (const [key, value] of Object.entries(price?.bpi)) {
+                a.push(`${key} - ${Number(value).toLocaleString()} THB`);
+            }
+            const b = a.map(x => <ul className="nobull"><li> {x} </li></ul>)
+            return b;
     }
 
+
     const render = () => {
-        if (loading)
-            return <p className='text-2xl'>Loading ...</p>
-        else if (error)
+        if (error) {
             return <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
-        else
+        } else if(loading){
+            return <p className='text-2xl'>Loading ...</p>
+        }
+        else if(have){
             return (
                 <div>
                     <p className='text-xl font-semibold'> ( From {s} To {e} )</p>
@@ -75,6 +78,8 @@ const HistoryResult = () => {
                 </div>
 
             )
+        }
+
     }
 
 
